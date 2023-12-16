256db4ad93d2acc05f9409f8b73cdf09ea383c13
priyankamadhwai <153282548+priyankamadhwai@users.noreply.github.com>
2023-12-16 15:02:49 +0530

    task2

---
+++ todolist.js
@@ -0,0 +1,32 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+ function addTask(){
+if(inputbox.value===''){
+alert("You must write something!");
+}
+else{
+let li=document.createElement("li");
+li.innerHTML=inputbox.value;
+listContainer.appendChild(li);
+
+let span=document.createElement("span");
+span.innerHTML="\u00d7";
+li.appendChild(span);
+}
+inputbox.value='';
+saveData();
+}
+listcontainer.addEventListener("click",function(e){
+if(e.target.tagName=="LI"){
+e.target.classList.toggle("checked");
+}
+else if(e.target.tagName==="SPAN"){
+e.target.parentElement.remove();
+saveData();
+}
+},false);
+
+function saveData()
+{
+localstorage.setItem("data",listcontainer.innerHTML);
+}
